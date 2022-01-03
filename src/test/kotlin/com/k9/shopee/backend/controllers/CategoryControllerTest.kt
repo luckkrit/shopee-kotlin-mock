db15aa59be3391c539a60459c3f42b5f7f5a2e13
Krit Chomaitong <luckkrit@hotmail.com>
2022-01-03 21:49:00 +0700

    Use WebTestClient instead.

--- src/test/kotlin/com/k9/shopee/backend/controllers/CategoryControllerTest.kt
+++ src/test/kotlin/com/k9/shopee/backend/controllers/CategoryControllerTest.kt
@@ -1,55 +1,35 @@
 package com.k9.shopee.backend.controllers
 
 import com.k9.shopee.backend.dtos.category.CategoryDto
-import org.junit.jupiter.api.*
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.web.server.LocalServerPort
-import org.springframework.http.HttpStatus
-import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.toEntity
-import reactor.core.publisher.Mono
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBodyList
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-internal class CategoryControllerTest() {
+internal class CategoryControllerTest(@Autowired private val webTestClient: WebTestClient) {
 
-    @LocalServerPort
-    var port: Int = 0
     private val log = LoggerFactory.getLogger(javaClass)
-    private lateinit var webClient: WebClient
 
-    private fun baseUri(): String = "http://localhost:$port/api"
-    private fun getWebClient(): WebClient = WebClient.builder().baseUrl(baseUri()).build()
-
-    @BeforeAll
-    fun initWebClient() {
-        webClient = getWebClient()
-        log.info("init web client")
-    }
 
     /**
-     * An example of webclient
+     * An example of WebClient, See
      * https://www.fabrizioduroni.it/2020/12/23/rest-template-webclient-spring-boot/
+     * anyway, WebTestClient is better for testing purposes, See
+     * https://spring.getdocs.org/en-US/spring-framework-docs/docs/testing/integration-testing/webtestclient.html#kotlin:kotlin-spring-projects-in-kotlin:testing.adoc#kotlin-webtestclient-issue
      */
     @Test
     @Order(1)
     fun getAllCategories() {
-        val result = webClient
-            .get()
-            .uri("/products/categories")
-            .retrieve()
-            .onStatus(HttpStatus::is4xxClientError) { Mono.error(RuntimeException("4XX Error ${it.statusCode()}")) }
-            .onStatus(HttpStatus::is5xxServerError) { Mono.error(RuntimeException("5XX Error ${it.statusCode()}")) }
-            .toEntity<List<CategoryDto>>().block()
-        assertNotNull(result)
-        assertEquals(result?.statusCode, HttpStatus.OK)
-        assertEquals(4, result?.body?.size)
-
+        webTestClient.get().uri("/products/categories").exchange().expectStatus().isOk.expectBodyList<CategoryDto>()
+            .hasSize(4)
     }
 
 }
